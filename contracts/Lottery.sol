e60dd879cce651690b20a44f6ac3162a174c5007
mka <mka281@icloud.com>
2018-08-15 14:51:41 -0700

    Enter the lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     function Lottery() public {
         manager = msg.sender;
+    }
+
+    function enter() public payable{
+      players.push(msg.sender);
     }
 }
