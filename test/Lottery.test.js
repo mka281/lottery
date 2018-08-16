d223a3723af54d7be539aca0ddf67a048f474658
mka <mka281@icloud.com>
2018-08-16 08:43:21 -0700

    Use try-catch to require a min amount of ether on enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -38,7 +38,7 @@ describe("Lottery contract", () => {
     assert.equal(1, players.length);
   });
 
-  it("allows one account to enter", async () => {
+  it("allows multiple accounts to enter", async () => {
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei("0.02", "ether")
@@ -60,5 +60,17 @@ describe("Lottery contract", () => {
     assert.equal(accounts[1], players[1]);
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length);
+  });
+
+  it("requires a minimum amount of ether to enter", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
   });
 });
