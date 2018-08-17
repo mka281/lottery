3256f2a92220e175a04bb553b8e0c2022203a4ea
mka <mka281@icloud.com>
2018-08-16 21:13:29 -0700

    fix: bytecode data in deploy function

--- deploy.js
+++ deploy.js
@@ -3,19 +3,18 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "negative employ record meadow swamp destroy page surprise upset burden afford flee",
-  "https://rinkeby.infura.io/rI75cuEXPEPQ4xWFzoja"
+  "verify belt float only coach purity logic rural afford mouse lock blind",
+  "https://rinkeby.infura.io/v3/df09796ce2554b5290ec72b48535f792"
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
   console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+    .deploy({ data: "0x" + bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 
   console.log("Contract deployed to", result.options.address);
